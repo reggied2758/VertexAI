f321f62546733795b83d022a0ab70854517b7b21
Davis <sfdss@gmail.com>
2026-07-19 21:52:15 +0100

    update

--- main.tf
+++ main.tf
@@ -6,12 +6,13 @@ provider "aws" {
 resource "aws_s3_bucket" "terraformstatefolder" {
   bucket = "terraformstatefolder"
 }
- 
+
 terraform {
   backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terraformstatefolder"
-    key            = "global/s3/terraform.tfstate"
-    region         = "eu-west-2"
+    bucket = "remote-state-bucket-reggie"
+    key    = "prd/terraform.tfstate"
+    region = "eu-west-2"
+    encrypt = true
   }
+
 }
