b0a83fafeb1babb189544537448aa5e109bb2785
Davis <sfdss@gmail.com>
2026-07-19 13:37:52 +0100

    update

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
 provider "aws" {
 
-region = "eu-west-1"
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "terraformstatefolder" {
+  bucket = "terraformstatefolder"
+}
+ 
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformstatefolder"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
 }
